2f2fe8973130de9d9a2bab6e969d5328c9b0be50
AdamMansell <adam_mansell@healthsaas.net>
2022-06-06 09:23:05 -0700

    Navbar Basic Skeleton setup with styled component. Sign In now routes to proper page

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 import {Link as LinkRouter} from 'react-router-dom'
-// import {Link as LinkScroll} from 'react-scroll'
+import {Link as LinkScroll} from 'react-scroll'
 
 export const Nav = styled.nav`
  background: black;
@@ -17,7 +17,7 @@ export const Nav = styled.nav`
  @media screen and (max-width: 960px) {
     transition: 0.8s all ease;
  }
-`
+`;
 
 
 export const NavbarContainer = styled.nav`
@@ -28,9 +28,10 @@ export const NavbarContainer = styled.nav`
  width: 100%;
  padding: 0 24px;
  max-width: 1100px;
-`
+`;
+
 export const NavLogo = styled(LinkRouter)`
- color: red;
+ color: #fff;
  justify-self: flex-start;
  cursor: pointer;
  font-size: 1.5rem;
@@ -39,7 +40,7 @@ export const NavLogo = styled(LinkRouter)`
  margin-left: 24px;
  font-weight: bold;
  text-decoration: none;
-`
+`;
 
 export const MobileIcon = styled.div`
  display: none;
@@ -52,5 +53,65 @@ export const MobileIcon = styled.div`
      transform: translate(-100%, 60%);
      font-size: 1.8rem;
      cursor: pointer;
+     color: #fff;
  }
-`
+`;
+
+export const NavMenu = styled.ul`
+ display: flex;
+ align-items: center;
+ list-style: none;
+ text-align: center;
+ margin-right: -22px;
+
+ @media screen and (max-width: 768px) {
+     display: none;
+ }
+ `;
+
+ export const NavItem = styled.li`
+  height: 80px;
+ `;
+
+ export const NavLinks = styled(LinkScroll)`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+
+  &.active {
+      border-bottom: 3px solid #01bf71;
+  }
+ `;
+
+ export const NavBtn = styled.nav`
+  display: flex;
+  align-items: center;
+
+  @media screen and (max-width: 768px) {
+      display: none;
+  }
+ `;
+
+ export const NavBtnLink = styled(LinkRouter)`
+  border-radius: 50px;
+  background: #01bf71;
+  white-space: nowrap;
+  padding: 10px 22px;
+  color: #010606;
+  font-size: 16px;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text decoration: none;
+
+  &:hover {
+      transition: all 0.2s ease-in-out;
+      background #fff;
+      color: #010606;
+  }
+ `;
