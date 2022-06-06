bea71a631e7ad8af7f460bf7d68ddb494bd299e5
AdamMansell <adam_mansell@healthsaas.net>
2022-06-06 11:29:24 -0700

    Added pages index.js file in order to properly organize App.js file

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Sidebar from '../components/Sidebar'
+import Navbar from '../components/Navbar'
+
+const Home = () => {
+  return (
+    <>
+    <Sidebar />
+    <Navbar />
+    </>
+  )
+}
+
+export default Home
